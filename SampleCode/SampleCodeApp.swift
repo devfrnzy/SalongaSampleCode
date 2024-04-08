49a8bcf52d92f4814317d69179af3deaf2a11269
Emmanuel Salonga <devfrnzy@gmail.com>
2024-04-09 01:41:03 +0800

    Initial Commit

---
+++ SampleCode/SampleCodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleCodeApp.swift
+//  SampleCode
+//
+//  Created by Emmanuel Salonga on 4/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SampleCodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
